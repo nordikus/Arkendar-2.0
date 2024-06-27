9b23c8bd1004498561ab2882ec126e8a52531cce
nordikus <egshik@gmail.com>
2024-06-27 21:38:36 +0300

    made redirect after registration

--- src/components/authentificationpages/Registration.jsx
+++ src/components/authentificationpages/Registration.jsx
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import {register} from "../../redux/authSlice";
 import MStyles from "../../MainStyles.module.scss";
+import {useNavigate} from "react-router-dom";
 const Registration= () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const dispatch = useDispatch();
+    const [isRegistered, setIsRegistered] = useState(false);
+    const navigate=useNavigate();
     const handleSubmit = (event) => {
         event.preventDefault();
         dispatch(register({ username, email, password }))
+        setIsRegistered(true);
     };
 
     return (
@@ -20,9 +24,10 @@ const Registration= () => {
             <label>Email:</label>
             <input required="true" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             <label>Пароль:</label>
-            <input  required="true" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <input required="true" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <button type="submit">Зарегистрироваться</button>
         </form>
+            {isRegistered && navigate("/login", { replace: true })}
         </div>
     );
 };
